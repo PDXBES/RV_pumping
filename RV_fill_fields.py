e640cd065335b8af8a7424f65bfd10b752c62313
dashney <daniel.ashney@portlandoregon.gov>
2021-12-20 16:06:44 -0800

    current code is filling geometry related fields from an intersect - could set up a try/except with edit session rollback but perhaps not really necessary right now?

--- RV_fill_fields.py
+++ RV_fill_fields.py
@@ -1,23 +1,47 @@
+import config
+import arcpy
 
 
-
-sect = arcpy.analysis.Intersect((target_feature, intersecting_list), r"memory\sect", "ALL")
-
-
-value_dict = {}
 def get_field_value_as_dict(input, value_field):
+    value_dict = {}
     with arcpy.da.SearchCursor(input, ("OBJECTID", value_field)) as cursor:
         for row in cursor:
             value_dict[row[0]] = row[1]
+    #print(value_dict)
     return value_dict
 
 def assign_field_value_from_dict(input_dict, target, target_field):
     with arcpy.da.UpdateCursor(target, ("OBJECTID", target_field)) as cursor:
         for row in cursor:
             if row[0] in input_dict.keys():
+                #print(str(row[1]) + " = " + str(input_dict[row[0]]))
                 row[1] = input_dict[row[0]]
             cursor.updateRow(row)
 
 def get_and_assign_field_value(source, source_field, target, target_field):
-	value_dict = get_field_value_as_dict(source, source_field)
-	assign_field_value_from_dict(value_dict, target, target_field)
+    value_dict = get_field_value_as_dict(source, source_field)
+    assign_field_value_from_dict(value_dict, target, target_field)
+
+
+# -------------------------------------------------------------------------
+print("STARTING PROCESS to fill out location fields")
+
+intersecting_list = [config.RV_pumping_fs, config.sextants, config.zipcodes, config.wsheds, config.nhoods, config.basins]
+print("intersecting all features")
+sect = arcpy.analysis.Intersect(intersecting_list, r"memory\sect", "ALL")
+
+#field_dict = {source_field:target_field} eg the field in the zipcode layer:the field in the feature service
+field_dict = {'ZIPCODE_1':'Zipcode','Sextant_1':'Sextant','NAME_1':'Neighborhood', 'WATERSHED_1': 'Watershed', 'BASIN_ID_1': 'Basin_ID'}
+
+for key in field_dict.keys():
+    print("filling field - " + str(field_dict[key]))
+    get_and_assign_field_value(sect, key, config.RV_pumping_fs, field_dict[key])
+
+print("filling Age")
+with arcpy.da.UpdateCursor(config.RV_pumping_fs, ['created_date', 'DOB', 'Age']) as cursor:
+    for row in cursor:
+        if row[1] is not None:
+            row[2] = row[0].year - row[1].year
+        cursor.updateRow(row)
+
+print("PROCESS ENDED")
--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 
 import os
-import sys
-import arcpy
+
 
 sde_egh_public = r"\\oberon\grp117\DAshney\Scripts\connections"
 
 EGH_PUBLIC = os.path.join(sde_egh_public, "egh_public on gisdb1.rose.portland.local.sde")
 
+#reference connections
 sextants = EGH_PUBLIC + r"\EGH_Public.ARCMAP_ADMIN.sextants_pdx"
 zipcodes = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.zipcode_metro"
 wsheds = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.watersheds_topography_bes_pdx"
 nhoods = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.neighborhoods_pdx"
 basins = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.sewer_basins_bes_pdx"
 
+RV_pumping_fs = "https://services.arcgis.com/quVN97tn06YNGj9s/arcgis/rest/services/RV_pumping_sites/FeatureServer/0"
