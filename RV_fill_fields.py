369c902b3e930c066a96b7304934322affad7a13
dashney <daniel.ashney@portlandoregon.gov>
2022-01-03 11:46:22 -0800

    added util, adding logging function to util, added block group to filled fields in main

--- RV_fill_fields.py
+++ RV_fill_fields.py
@@ -1,4 +1,5 @@
 import config
+import utility
 import arcpy
 import os
 
@@ -25,21 +26,32 @@ def get_and_assign_field_value(source, source_field, target, target_field):
 
 
 # -------------------------------------------------------------------------
-print("STARTING PROCESS to fill out location fields")
+#print("STARTING PROCESS to fill out location fields")
+
+log_obj = utility.Logger(config.log_file)
+
+log_obj.info("STARTING PROCESS - fill out location fields".format())
 
 for key, value in config.fc_field_dict.items():
     intersecting_list = (config.RV_pumping_fs, key)
-    print("intersecting features - " + str(value[1]))
+    #print("intersecting features - " + str(value[1]))
+    msg1 = "intersecting features - " + str(value[1])
+    log_obj.info(msg1.format())
     in_memory_name = r"memory\sect_" + value[1]
     sect = arcpy.analysis.Intersect(intersecting_list, in_memory_name, "ALL")
-    print("filling field - " + str(value[1]))
+    #print("filling field - " + str(value[1]))
+    msg2 = "filling field - " + str(value[1])
+    log_obj.info(msg2.format())
     get_and_assign_field_value(sect, value[0], config.RV_pumping_fs, value[1])
 
-print("filling Age")
+#print("filling Age")
+log_obj.info("- filling Age".format())
 with arcpy.da.UpdateCursor(config.RV_pumping_fs, ['created_date', 'DOB', 'Age']) as cursor:
     for row in cursor:
         if row[1] is not None:
             row[2] = row[0].year - row[1].year
         cursor.updateRow(row)
 
-print("PROCESS ENDED")
+#print("PROCESS ENDED")
+
+log_obj.info("PROCESS COMPLETE - fill out location fields".format())
--- config.py
+++ config.py
@@ -12,11 +12,17 @@ zipcodes = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.zipcode_metro"
 wsheds = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.watersheds_topography_bes_pdx"
 nhoods = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.neighborhoods_pdx"
 basins = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.sewer_basins_bes_pdx"
+block_groups = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.census_blockgroup_2010_metro"
 
 RV_pumping_fs = "https://services.arcgis.com/quVN97tn06YNGj9s/arcgis/rest/services/RV_pumping_sites/FeatureServer/0"
 
-fc_field_dict = {zipcodes:('ZIPCODE_1','Zipcode'),
-                 sextants:('Sextant_1','Sextant'),
-                 nhoods:('NAME','Neighborhood'),
-                 wsheds:('WATERSHED_1', 'Watershed'),
-                 basins:('BASIN_ID_1', 'Basin_ID')}
+fc_field_dict = {
+                 zipcodes:('ZIPCODE_1','Zipcode')
+                 ,sextants:('Sextant_1','Sextant')
+                 ,nhoods:('NAME','Neighborhood')
+                 ,wsheds:('WATERSHED_1', 'Watershed')
+                 ,basins:('BASIN_ID_1', 'Basin_ID')
+                 ,block_groups:('FIPS', 'BlockGroup_ID')
+                }
+
+log_file = r"\\besfile1\ISM_PROJECTS\Work_Orders\WO_9857_A_Chomowicz\log\data_load_log"
